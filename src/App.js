be60b2c9f6a4d223c2eb74f2cc8c375d615ebab0
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-17 18:50:55 -0500

    basic footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 // import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Header></Header>
         <Navigation></Navigation>
       </main>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Copyright © Adam Keyser 2020
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
