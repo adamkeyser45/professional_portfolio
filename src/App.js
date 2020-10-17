206142becbda51651bf31bc09615caa3746120b1
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-17 18:22:21 -0500

    basic Header done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 // import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Adam Keyser
-        </p>
-      </header>
+    <div>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Adam Keyser</h1>
+        </header>
+    )
+}
+
+export default Header;
