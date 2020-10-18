581d7440cf0db28f8889366faaf8669afcf3dacb
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-18 14:35:11 -0500

    About me page done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 function About() {
     return (
-        <div>
-            This is the About Page!
+        <div class="jumbotron">
+            <h1 class="display-3">About Me</h1>
+            <p class="lead">
+                As a web developer, it is my goal to help create websites, applications, 
+                and tools that can be used by the masses to help improve their lives.
+            </p>
+            <hr class="my-4" />
+            <h4>My Journey</h4>
+            <p>
+                My professional journey to coding began differently than most programmers. It started when I graduated college with a degree 
+                in Elementary Education. My first teaching position as a 4th grade ELA and Social Studies teacher was at Hazelwood Elementary in 
+                Clarksville, Tennessee. After working there for two years, I found a similar job in Nashville, TN. It was my two years at Inglewood 
+                Elementary where I really took interest in coding.
+            </p>
+            <p>
+                Inglewood is a STEAM Magnet School. Science, Technology, Engineering, Arts, and Math were to be incorporated in every aspect of 
+                the school. Even the playground equipment outside could be programmed with a basic coding language, Scratch, to make sounds and 
+                noises when interacted with by students. I began to see what amazing things could be done when you can incorporate education and 
+                programming.
+            </p>
         </div>
     )
 }
