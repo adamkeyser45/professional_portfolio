2c42e4e0e4def007743a09c471468fdd7e9a62bd
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-18 11:12:42 -0500

    added bootstrap links and scripts

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,48 @@
 import React, { useState } from 'react';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
 
-function Navigation(props) {
-    const {
-        tabs = []
-    } = props;
-    const [currentTab, setCurrentTab] =useState(tabs[0]);
+// function Navigation(props) {
+//     const {
+//         tabs = []
+//     } = props;
+//     const [currentTab, setCurrentTab] =useState(tabs[0]);
 
-    function tabSelected(name) {
-        console.log(`${name} was clicked!`)
-    }
+//     function tabSelected(name) {
+//         console.log(`${name} was clicked!`)
+//     }
 
+//     return (
+//         <nav>
+//             <ul>
+//                 {tabs.map((tab) => (
+//                     <li
+//                         key={tab.name}
+//                     >
+//                         <span onClick={() => tabSelected(tab.name)}>
+//                             {tab.name}
+//                         </span>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </nav>
+//     );
+// }
+
+function Navigation() {
     return (
-        <nav>
-            <ul>
-                {tabs.map((tab) => (
-                    <li
-                        key={tab.name}
-                    >
-                        <span onClick={() => tabSelected(tab.name)}>
-                            {tab.name}
-                        </span>
-                    </li>
-                ))}
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <a className="nav-item nav-link" href="#about">About Me</a>
+                    <a className="nav-item nav-link" href="#portfolio">Portfolio</a>
+                    <a className="nav-item nav-link" href="#contact">Contact</a>
+                    <a className="nav-item nav-link" href="#resume" >Resume</a>
+                </div>
+            </div>
         </nav>
     );
 }
