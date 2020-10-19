fdf509717333c6959f93fc09c5dabd6ff8c8cf90
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-18 20:03:42 -0500

    added resume download

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,28 +3,38 @@ import React from 'react';
 function Resume() {
     return (
         <div className="card m-5" style={{border: "none"}}>
-            <ul className="list-unstyled">
-                <li>Front-End Proficiencies</li>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>jQuery</li>
-                        <li>Responsive Design</li>
-                        <li>React</li>
-                        <li>Bootstrap</li>
-                    </ul>
-                    <br/>
-                <li>Back-End Proficiencies</li>
-                    <ul>
-                        <li>APIs</li>
-                        <li>Node</li>
-                        <li>Express</li>
-                        <li>MySQL, Sequelize</li>
-                        <li>REST</li>
-                        <li>MongoDB, Mongoose</li>
-                    </ul>
-            </ul>
+            <div className="row">
+                <ul className="list-unstyled" style={{width: "33%"}}>
+                    <li className="h2">Front-End Proficiencies</li>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>jQuery</li>
+                            <li>Responsive Design</li>
+                            <li>React</li>
+                            <li>Bootstrap</li>
+                        </ul>
+                </ul>
+                <ul className="list-unstyled" style={{width: "33%"}}>
+                    <li className="h2">Back-End Proficiencies</li>
+                        <ul>
+                            <li>APIs</li>
+                            <li>Node</li>
+                            <li>Express</li>
+                            <li>MySQL, Sequelize</li>
+                            <li>REST</li>
+                            <li>MongoDB, Mongoose</li>
+                        </ul>
+                </ul>
+
+                <div className="card text-center" style={{width: "33%", border: "none"}}>
+                    <h2>Click here to download my Full Resume</h2>
+                    <a href={require("../../assets/Adam_Keyser_Resume.pdf")} download>
+                    <i class="far fa-file-pdf fa-7x mx-auto" style={{width: "100px"}}></i>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
