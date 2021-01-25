d5b6e998fc0250e4f7b544ae39cdea5c7fda542b
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2021-01-24 21:32:10 -0600

    fixed responsiveness of resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,25 +6,25 @@ function Resume() {
             <div className="row">
                 <ul className="list-unstyled col-12 col-md-4" >
                     <li className="h2 m-2 text-center">Front-End Proficiencies</li>
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>jQuery</li>
-                            <li>Responsive Design</li>
-                            <li>React</li>
-                            <li>Bootstrap</li>
+                        <ul className="list-group">
+                            <li className="list-group-item mx-3 text-center h5">HTML</li>
+                            <li className="list-group-item mx-3 text-center h5">CSS</li>
+                            <li className="list-group-item mx-3 text-center h5">JavaScript</li>
+                            <li className="list-group-item mx-3 text-center h5">jQuery</li>
+                            <li className="list-group-item mx-3 text-center h5">Responsive Design</li>
+                            <li className="list-group-item mx-3 text-center h5">React</li>
+                            <li className="list-group-item mx-3 text-center h5">Bootstrap</li>
                         </ul>
                 </ul>
                 <ul className="list-unstyled col-12 col-md-4" >
                     <li className="h2 m-2 text-center">Back-End Proficiencies</li>
-                        <ul>
-                            <li>APIs</li>
-                            <li>Node</li>
-                            <li>Express</li>
-                            <li>MySQL, Sequelize</li>
-                            <li>REST</li>
-                            <li>MongoDB, Mongoose</li>
+                        <ul className="list-group">
+                            <li className="list-group-item mx-3 text-center h5">APIs</li>
+                            <li className="list-group-item mx-3 text-center h5">Node</li>
+                            <li className="list-group-item mx-3 text-center h5">Express</li>
+                            <li className="list-group-item mx-3 text-center h5">MySQL, Sequelize</li>
+                            <li className="list-group-item mx-3 text-center h5">REST</li>
+                            <li className="list-group-item mx-3 text-center h5">MongoDB/Mongoose</li>
                         </ul>
                 </ul>
 
